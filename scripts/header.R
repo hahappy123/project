b4b5535fdaf2bab6c5df310020e98e0429295c2d
hahappy123 <3045382472@qq.com>
2025-03-12 11:41:39 +0800

    header

---
+++ scripts/header.R
@@ -0,0 +1,12 @@
+# HEADER --------------------------------------------
+#
+# Author:     zhang-yj
+# Copyright     Copyright 2025 - zhang-yj
+# Email:      3045382472@qq.com
+#
+# Date:     2025-03-12
+#
+# Script Description:    eco-class
+# SETUP ------------------------------------
+cat("\014")                 # Clears the console
+rm(list = ls())             # Remove all variables of the work space
